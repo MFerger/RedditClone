e0f991a7bddc91828b129e09e3e553dc6dbf226c
Michael Ferger <mferger92@gmail.com>
2016-04-29 10:31:54 -0600

    Fixed upvote sorting bug, added some styling to modal

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ app.controller('MainController', function($scope){
   $scope.vm = {};
   $scope.data = [
     {
-      title:  'Aspen',
+      id: 1,
+      title:  'spen',
       image: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
       author: "Bud Anin",
@@ -13,6 +14,7 @@ app.controller('MainController', function($scope){
       comments: []
     },
     {
+      id: 2,
       title:  'Boulder',
       image: 'http://im.rediff.com/news/2016/mar/01smith1.jpg',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
@@ -22,6 +24,7 @@ app.controller('MainController', function($scope){
       comments: []
     },
     {
+      id: 3,
       title:  'Tel Aviv',
       image: 'http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
@@ -29,16 +32,36 @@ app.controller('MainController', function($scope){
       date: new Date(),
       votes: 4,
       comments: []
+    },
+    {
+      id: 4,
+      title:  'spen',
+      image: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Bud Anin",
+      date: new Date(),
+      votes: 2,
+      comments: []
+    },
+    {
+      id: 5,
+      title:  'spen',
+      image: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Bud Anin",
+      date: new Date(),
+      votes: 6,
+      comments: []
     }
   ];
   $scope.sort = function (sorted){
     $scope.vm.sort = sorted;
-  }
-  $scope.up = function(post) {
-    $scope.data[post].votes++
-  }
-  $scope.down = function (post) {
-    $scope.data[post].votes--
-  }
+  };
 
+  $scope.up = function(post) {
+    post.votes++
+  };
+  $scope.down = function(post) {
+    post.votes--
+  };
 });
