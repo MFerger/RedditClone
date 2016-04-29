15badcfb24097a7c9a282cc3e6d0d4c121b3d46d
Michael Ferger <mferger92@gmail.com>
2016-04-28 19:36:35 -0600

    Initial commit, added search bar and sorting functionality

---
+++ app.js
@@ -0,0 +1,37 @@
+var app = angular.module('form', ['ngAnimate']);
+
+app.controller('MainController', function($scope){
+  $scope.vm = {};
+  $scope.data = [
+    {
+      title:  'Aspen',
+      image: 'http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Bud Anin",
+      date: new Date(),
+      votes: 3,
+      comments: []
+    },
+    {
+      title:  'Boulder',
+      image: 'http://im.rediff.com/news/2016/mar/01smith1.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Nick MacIntyre",
+      date: new Date(),
+      votes: 2,
+      comments: []
+    },
+    {
+      title:  'Tel Aviv',
+      image: 'http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula fermentum neque et congue. Vestibulum sed nulla dictum, accumsan nulla a, mattis tortor. Mauris purus risus, maximus ac risus quis, suscipit sagittis lectus. Nulla sit amet pulvinar lorem. Integer in sapien finibus, scelerisque lacus eget, malesuada diam.",
+      author: "Coutrney and Mike",
+      date: new Date(),
+      votes: 4,
+      comments: []
+    }
+  ];
+  $scope.sort = function (sorted){
+    $scope.vm.sort = sorted;
+  }
+});
