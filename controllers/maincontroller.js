a1c740d2212e3f22af1f2d8a989eacd3f26df860
Michael Ferger <mferger92@gmail.com>
2016-05-10 22:02:10 -0600

    moved functions for app from main controller into they're respective directives

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,47 +1,47 @@
 angular.module('form')
   .controller('MainController', function($scope, postService) {
-  $scope.vm = {};
-  $scope.newComments = {};
-  $scope.newPostVisible = false;
-  $scope.newPost = {};
-  $scope.data = postService.posts;
+  // $scope.vm = {};
+  // $scope.newComments = {};
+  // $scope.newPostVisible = false;
+  // $scope.newPost = {};
+  // $scope.data = postService.posts;
 
-  $scope.sort = function(sorted) {
-    $scope.vm.sort = sorted;
-  };
+  // $scope.sort = function(sorted) {
+  //   $scope.vm.sort = sorted;
+  // };
 
-  $scope.up = function(post) {
-    post.votes++
-  };
+  // $scope.up = function(post) {
+  //   post.votes++
+  // };
+  //
+  // $scope.down = function(post) {
+  //   post.votes--
+  // };
 
-  $scope.down = function(post) {
-    post.votes--
-  };
+  // $scope.newComment = function(post) {
+  //   post.newCommentVisible = !post.newCommentVisible;
+  // };
+  //
+  // $scope.showComments = function(post) {
+  //   post.showComment = !post.showComment;
+  // };
 
-  $scope.newComment = function(post) {
-    post.newCommentVisible = !post.newCommentVisible;
-  };
-
-  $scope.showComments = function(post) {
-    post.showComment = !post.showComment;
-  };
-
-  $scope.addPost = function(post) {
-    console.log("post",post);
-      post.date = new Date();
-      post.votes = 0;
-      post.comments = [];
-      post.showComment = false;
-      post.newCommentVisible = false;
-      $scope.data.push(post);
-      $scope.vm.newPostVisible = false;
-      $scope.newPost = {};
-    };
-$scope.addComment = function(post, comment, index) {
-  if (comment.author && comment.text) {
-    post.comments.push(comment);
-    post.newCommentVisible = false;
-    $scope.newComment[index] = null;
-  }
-};
+  // $scope.addPost = function(post) {
+  //   console.log("post",post);
+  //     post.date = new Date();
+  //     post.votes = 0;
+  //     post.comments = [];
+  //     post.showComment = false;
+  //     post.newCommentVisible = false;
+  //     $scope.data.push(post);
+  //     $scope.vm.newPostVisible = false;
+  //     $scope.newPost = {};
+  //   };
+// $scope.addComment = function(post, comment, index) {
+//   if (comment.author && comment.text) {
+//     post.comments.push(comment);
+//     post.newCommentVisible = false;
+//     $scope.newComment[index] = null;
+//   }
+// };
 })
--- directives/postings/postings.js
+++ directives/postings/postings.js
@@ -1,7 +1,31 @@
 angular.module('form')
-  .directive('posting', function() {
+  .directive('posting', function($scope) {
   return {
     restrict: 'E',
     templateUrl: '/directives/postings/postings.html'
   }
+  $scope.up = function(post) {
+    post.votes++
+  };
+
+  $scope.down = function(post) {
+    post.votes--
+  };
+
+  $scope.newComment = function(post) {
+    post.newCommentVisible = !post.newCommentVisible;
+  };
+
+  $scope.showComments = function(post) {
+    post.showComment = !post.showComment;
+  };
+
+  $scope.addComment = function(post, comment, index) {
+    if (comment.author && comment.text) {
+      post.comments.push(comment);
+      post.newCommentVisible = false;
+      $scope.newComment[index] = null;
+    }
+  };
+  
 })
